00e1096efc57cdbb50fbd33cab321d025b3a48c1
Saygong <andreagentilini5@gmail.com>
2022-05-11 23:43:01 +0200

    Started unit testing with mocha and chai and added endpoint-overview.txt

--- src/routes/chat-routes.ts
+++ src/routes/chat-routes.ts
@@ -5,10 +5,17 @@ import {Chat} from "../models/chat" ;
 
 const router: Router = Router()
 
-router.get('/', (req: Request, res: Response, next: NextFunction) => {
+
+// Retrieving schema from model
+const chatSchema = require('../models/chat');
+const userSchema = require('../models/user');
+const matchSchema = require('../models/match');
+
+router.get('/chats', (req, res, next) => {
+
 	// Check if user is logged
     if (!req.user_id) { return res.status(401).send('Not authenticated'); }
-    
+
 	const { ids } = req.query;
     if (!ids) {
       return res.status(400).send('Bad Request');
@@ -16,16 +23,13 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 	// Retrieve userId from the parameters of the request
 	const user = req.params.userId;
-	
+
 	// Find all the existing chat where an user is found inside the "users" list
-    Chat.find({ users: user })
+    userSchema.findById(user)
     .lean()
     .exec()
     .then(found => {
-        if (found.length === 0) {
-            return res.status(404).send('Chats not found');
-        }
-        res.json(found);
+        res.json(found.chats);
     })
 	.catch(next);
 })
