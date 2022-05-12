63a881fe8bd1de3d298ec64324fa0798c0541c33
skippa <881423@stud.unive.it>
2022-05-12 16:15:02 +0200

    ayo

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,11 +2,13 @@ import * as mongoose from "mongoose";
 import {Document, Model, Schema, SchemaTypes} from "mongoose";
 
 
-const statsSchema = new Schema({
+
+var statsSchema = new mongoose.Schema({
 
     elo: {
-        type: SchemaTypes.Number,
-        default: 0
+        type: mongoose.SchemaTypes.Number,
+        default: 0,
+        index: true
     }, 
     wins: {
         type: SchemaTypes.Number,
@@ -36,7 +38,8 @@ const userSchema = new Schema({
     username: {
         type: SchemaTypes.String,
         required: true, 
-        unique: true
+        unique: true, 
+        index: true
     },
 
     mail: {
@@ -73,3 +76,39 @@ const userSchema = new Schema({
 })
 
 export const User: Model<Document> = mongoose.model("User", userSchema)
+
+
+userSchema.methods.addFriends = function( data: [] ) : void {
+    for(var id of data){
+        if (id !== this._id)
+            this.friends.push(id)
+    }
+}
+
+userSchema.methods.setModerator = function() : void {
+    if(!this.isModerator())
+        this.role.push("moderator")
+}
+
+userSchema.methods.setAdmin = function() : void {
+    if (!this.isAdmin())
+        this.role.push("admin")
+}
+
+userSchema.methods.hasRole = function( role: string ) : boolean {
+    var value: boolean = false;
+    this.roles.array.forEach(element => {
+        if (element === role) value = true;
+    });
+    return value;
+}
+
+userSchema.methods.isModerator = function() : boolean {
+    return this.hasRole("moderator")
+}
+
+userSchema.methods.isAdmin = function( role: string ) : boolean {
+    return this.hasRole("admin")
+}
+
+
