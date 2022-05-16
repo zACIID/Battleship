57247f8c6fbb968117bff881a88e3c7e597e18e7
Saygong <andreagentilini5@gmail.com>
2022-05-16 16:08:37 +0200

    Added bcrypt and made major improvement to User and Match models

--- src/models/match.ts
+++ src/models/match.ts
@@ -2,6 +2,43 @@ import * as mongoose from "mongoose";
 import {Document, Model, Schema, Types, SchemaTypes} from "mongoose";
 import {Chat, IChat} from "./chat";
 
+
+/**
+ * Interface that represent a stats sub-document found in a Match document.
+ *
+ */
+ export interface IMatchStats {
+    winner: Types.ObjectId;
+    start_time: Date;
+    end_time: Date;
+    total_shots: number;
+    ships_destroyed: number;
+}
+
+export const MatchStatsSchema = new Schema<IMatchStats>({
+    winner: {
+        type: SchemaTypes.ObjectId
+    },
+    start_time: {
+        type: SchemaTypes.Date,
+        required: true
+    },
+    end_time: {
+        type: SchemaTypes.Date
+    },
+    total_shots: {
+        type: SchemaTypes.Number,
+        default: 0
+    },
+    ships_destroyed: {
+        type: SchemaTypes.Number,
+        default: 0
+    },
+
+})
+
+
+
 /**
  * Interface that represents a Match document.
  * Such document represents a match between two players and the two types
@@ -12,6 +49,7 @@ export interface IMatch extends Document {
     player_2: Types.ObjectId;
     players_chat: IChat;
     observers_chat: IChat;
+    stats: IMatchStats;
 }
 
 export const MatchSchema = new Schema<IMatch>({
@@ -21,7 +59,10 @@ export const MatchSchema = new Schema<IMatch>({
 
     players_chat: Chat,
 
-    observers_chat: Chat
+    observers_chat: Chat,
+
+    stats: MatchStatsSchema
+
 });
 
 export const Match: Model<IMatch> = mongoose.model("Match", MatchSchema, "matches");
