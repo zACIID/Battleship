7d968b08ca63fbd0c7633730133126817b27ef11
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-05-18 01:53:53 +0200

    refactor: part2 of the previous commit

---
+++ src/models/user-stats.ts
@@ -0,0 +1,46 @@
+import {Schema, SchemaTypes} from 'mongoose';
+
+/**
+ * Interface that represent the stats of some user of the system.
+ */
+export interface UserStats {
+    elo: number;
+    topElo: number;
+    wins: number;
+    losses: number;
+    shipsDestroyed: number;
+    totalShots: number;
+    hits: number;
+}
+
+export const StatsSchema = new Schema<UserStats>({
+    elo: {
+        type: SchemaTypes.Number,
+        default: 0,
+        index: true,
+    },
+    topElo: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+    wins: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+    losses: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+    shipsDestroyed: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+    totalShots: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+    totalHits: {
+        type: SchemaTypes.Number,
+        default: 0,
+    },
+});
