9a333ae05695fe3161aa90e4c6984a2be2b32003
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-05-26 23:28:14 +0200

    feat(matchmaking): started implementation of matchmaking

---
+++ src/models/matchmaking/queue-entry.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, SchemaTypes, Types } from 'mongoose';
+
+/**
+ * Interface that represent the stats of some user of the system.
+ */
+export interface QueueEntry {
+    userId: Types.ObjectId;
+    elo: number;
+}
+
+/**
+ * Interface that represent an entry of a MatchmakingQueue collection
+ */
+export interface QueueEntrySubDocument extends QueueEntry, Types.EmbeddedDocument {}
+
+export const QueueEntrySchema = new Schema<QueueEntrySubDocument>(
+  {
+      userId: {
+          type: SchemaTypes.ObjectId,
+          required: [true, "User id is required"]
+      },
+      elo: {
+          type: SchemaTypes.Number,
+          required: [true, "Elo is required"]
+      }
+  }, { _id: false }
+);
---
+++ src/models/matchmaking/queue.ts
@@ -0,0 +1,26 @@
+import { Document, Schema, SchemaTypes, Types } from 'mongoose';
+import { QueueEntry, QueueEntrySchema, QueueEntrySubDocument } from "./queue-entry";
+import * as mongoose from "mongoose";
+
+/**
+ * Interface that represent a matchmaking queue
+ */
+export interface MatchmakingQueue {
+    queue: QueueEntry[];
+}
+
+/**
+ * Interface that represent the document of a matchmaking queue
+ */
+export interface MatchmakingQueueDocument extends MatchmakingQueue, Document {
+    queue: QueueEntrySubDocument[];
+}
+
+export const MatchmakingQueueSchema = new Schema<MatchmakingQueueDocument>({
+    queue: {
+        type: [QueueEntrySchema],
+        default: []
+    }
+});
+
+export const MatchmakingQueueModel = mongoose.model("MatchmakingQueue", MatchmakingQueueSchema, "MatchmakingQueues");
