12019cd40b61edf9e558438088c4a890e0263542
Saygong <andreagentilini5@gmail.com>
2022-05-26 14:41:59 +0200

    fix: Added some TODOs and renamed a module

--- mandrilli-angular-app/src/app/core/types/user.ts
+++ mandrilli-angular-app/src/app/core/types/user.ts
@@ -1,6 +1,8 @@
 import { Relationship } from './relationship';
 import { UserRoles } from './../../../../../src/models/user';
 import { Types } from "mongoose";
+// TODO waiting for merge
+//import { Notification } from './notification';
 
 export interface User{
     userId: Types.ObjectId;
