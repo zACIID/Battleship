16db01713526cf4b5dbeb4a7006c4a80d58f1694
skippa <881423@stud.unive.it>
2022-05-31 12:39:02 +0200

    Users client done

--- MandrilliWeb/src/app/core/api/user-api.ts
+++ MandrilliWeb/src/app/core/api/user-api.ts
@@ -1,56 +1,56 @@
 import { BaseAuthenticatedApi } from './base-api';
 import { User } from '../model/user/user';
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
 import { UserStats } from '../model/user/user-stats';
+import { Observable, throwError } from 'rxjs';
+import * as jwtdecode from 'jwt-decode';
+import {handleError, createOptions} from '../handler/ErrorsNdHeaders'
 
 /**
  * Class that handles communication with user-related endpoints
  */
 export class UserApi extends BaseAuthenticatedApi {
-    public constructor(baseUrl: string, authToken: string) {
+    private http: HttpClient;
+    private authToken: string;
+    public constructor(baseUrl: string, authToken: string, http: HttpClient) {
         super(baseUrl, authToken);
+        this.http = http;
+        this.authToken = authToken;
     }
 
-    public getUser(userId: string): User {
+
+    public getUser(userId: string): Observable<User> {
         const reqPath: string = `/api/users/${userId}`;
-
-        throw new Error("Not Implemented");
+        return this.http.get<User>(reqPath, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public getMultipleUsers(userIds: string[]): User[] {
+    public getMultipleUsers(userIds: string[]): Observable<User[]> {
         const reqPath: string = `/api/users`;
-
-        throw new Error("Not Implemented");
+        return this.http.get<User[]>(reqPath, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public deleteUser(userId: string): boolean {
-        // TODO non serve perché unico che cancella user è moderator tramite il suo endpoint?
-        //  oppure opzione che user si cancella dal sistema?
+    public deleteUser(userId: string): Observable<User> {
         const reqPath: string = `/api/users/${userId}`;
-
-        throw new Error("Not Implemented");
+        return this.http.delete<User>(reqPath, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public updatePassword(userId: string, newPassword: string): boolean {
+    public updatePassword(userId: string, newPassword: string): Observable<User> {
         const reqPath: string = `/api/users/${userId}/password`;
-
-        throw new Error("Not Implemented");
+        return this.http.put<User>(reqPath, newPassword, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public updateUsername(userId: string, newUsername: string): boolean {
+    public updateUsername(userId: string, newUsername: string): Observable<User> {
         const reqPath: string = `/api/users/${userId}/username`;
-
-        throw new Error("Not Implemented");
+        return this.http.put<User>(reqPath, newUsername, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public getStats(userId: string): UserStats {
+    public getStats(userId: string): Observable<UserStats> {
         const reqPath: string = `/api/users/${userId}/stats`;
-
-        throw new Error("Not Implemented");
+        return this.http.get<UserStats>(reqPath, createOptions({}, this.authToken)).pipe(handleError)
     }
 
-    public updateStats(userId: string, statsUpdate: UserStats): boolean {
+    public updateStats(userId: string, statsUpdate: UserStats): Observable<UserStats> {
         const reqPath: string = `/api/users/${userId}/stats`;
-
-        throw new Error("Not Implemented");
+        return this.http.put<UserStats>(reqPath, statsUpdate, createOptions({}, this.authToken)).pipe(handleError)
     }
 }
