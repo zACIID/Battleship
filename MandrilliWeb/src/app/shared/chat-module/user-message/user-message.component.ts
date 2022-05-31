cfec56d1f486e41f30167719fa4776da6375a327
Saygong <andreagentilini5@gmail.com>
2022-05-31 17:04:16 +0200

    feat: added usermessages and foreign messages

---
+++ MandrilliWeb/src/app/shared/chat-module/user-message/user-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'user-message',
+  templateUrl: './user-message.component.html',
+  styleUrls: ['./user-message.component.css']
+})
+export class UserMessageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
