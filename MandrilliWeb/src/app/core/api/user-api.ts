d863c278fe6a9f59b494a4713731600993adae45
skippa <881423@stud.unive.it>
2022-05-31 17:47:51 +0200

    services done: (not tested)

--- MandrilliWeb/src/app/core/api/user-api.ts
+++ MandrilliWeb/src/app/core/api/user-api.ts
@@ -2,11 +2,7 @@ import { BaseAuthenticatedApi } from './base-api';
 import { User } from '../model/user/user';
 import { HttpClient } from '@angular/common/http';
 import { UserStats } from '../model/user/user-stats';
-<<<<<<< HEAD
-import { Observable, throwError, catchError } from 'rxjs';
-=======
 import { Observable, catchError } from 'rxjs';
->>>>>>> Frontend
 import {handleError, createOptions} from '../handler/ErrorsNdHeaders'
 
 
