aadb9cd974edc5ef6e813bce71a34aec047185c7
Saygong <andreagentilini5@gmail.com>
2022-06-09 16:22:26 +0200

    feat: first implementation of observers-screen logic

--- MandrilliWeb/src/app/features/observers-view-module/observers-screen/observers-screen.component.ts
+++ MandrilliWeb/src/app/features/observers-view-module/observers-screen/observers-screen.component.ts
@@ -1,3 +1,7 @@
+import { ChatMessageListener } from './../../../core/events/listeners/chat-message';
+import { MatchApi } from './../../../core/api/handlers/match-api';
+import { Match } from './../../../core/model/match/match';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +10,67 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./observers-screen.component.css'],
 })
 export class ObserversScreenComponent implements OnInit {
-    constructor() {}
 
-    ngOnInit(): void {}
+
+    public matchShowedId: string = "";
+    public match?: Match;
+    public chatId: string = "";
+
+    constructor(
+        private route: ActivatedRoute,
+        private matchClient: MatchApi,
+        private chatMessageListener: ChatMessageListener,
+        private router: Router
+    ) {}
+
+    ngOnInit(): void {
+
+        try{
+            this.route.params.subscribe((params) => {
+                this.matchShowedId = params['id'];
+            });
+
+            this.matchClient.getMatch(this.matchShowedId).subscribe((data) => {
+                this.match = data;
+
+                this.chatId = data.observersChat;
+            })
+
+
+
+            /*   TODO 
+             *   Emit nella chat appena joinata ?  
+             *   E' l'emit che aggiunge lo user alla chat ?  
+             */ 
+
+
+            // It should force the chatBody component to refresh
+            this.chatMessageListener.listen(
+                () => { 
+                    if (this.match){
+                        this.chatId = this.match?.observersChat
+                    }
+                    else throw new Error("Observers chat id non existent");
+                });
+
+
+        }
+        catch(err){
+            console.log("An error occurred while retrieving the match: " + err);
+        }
+    }
+
+
+    public quitView(){
+
+        /*  TODO 
+         *  add logic here
+         *  we should remove the user from the chat
+         *  we should also unlisten the chat-message event 
+         */ 
+        
+        this.router.navigate(['/relationship']);
+    }
+
+    
 }
