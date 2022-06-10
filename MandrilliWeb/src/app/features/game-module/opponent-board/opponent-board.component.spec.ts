4a86f9f2d28ace7cfd911c3ad5a00ca59639d52a
Saygong <andreagentilini5@gmail.com>
2022-06-10 21:10:19 +0200

    feat: Added opponents-board component in order to proper show this board

---
+++ MandrilliWeb/src/app/features/game-module/opponent-board/opponent-board.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OpponentBoardComponent } from './opponent-board.component';
+
+describe('OpponentBoardComponent', () => {
+  let component: OpponentBoardComponent;
+  let fixture: ComponentFixture<OpponentBoardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OpponentBoardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OpponentBoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
