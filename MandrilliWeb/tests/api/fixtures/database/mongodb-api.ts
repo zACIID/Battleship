fa7c48f1c93049adb7c8ba99623a16c609f49bc8
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-06-11 11:31:23 +0200

    tests(api-integration): moved axios header setup to setup-jest.ts

--- MandrilliWeb/tests/api/fixtures/database/mongodb-api.ts
+++ MandrilliWeb/tests/api/fixtures/database/mongodb-api.ts
@@ -1,8 +1,5 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-// https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios
-axios.defaults.adapter = require('axios/lib/adapters/http');
-
 import * as dbUser from '../../../../../src/model/user/user';
 import * as dbMatch from '../../../../../src/model/match/match';
 import * as dbChat from '../../../../../src/model/chat/chat';
--- MandrilliWeb/tests/setup-jest.ts
+++ MandrilliWeb/tests/setup-jest.ts
@@ -1,3 +1,7 @@
 import 'jest-preset-angular/setup-jest';
+import axios from 'axios';
 
 jest.setTimeout(7500);
+
+// https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios
+axios.defaults.adapter = require('axios/lib/adapters/http');
