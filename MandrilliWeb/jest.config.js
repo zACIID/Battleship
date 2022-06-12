75517d510f34a3385ec1fcb8fa43d90d643816ea
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-06-13 00:44:20 +0200

    tests(socket.io-integration): added line in jest.config.js that allows socket.io tests to work

--- MandrilliWeb/jest.config.js
+++ MandrilliWeb/jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: ['html'],
     coverageDirectory: 'coverage/my-app',
+
+    // https://stackoverflow.com/questions/71421924/running-jest-in-angular13-unexpected-value-translatemodule-imported-by-the-mo
+    globalSetup: 'jest-preset-angular/global-setup',
 };
