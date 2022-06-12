bf291f64c022d190b6ade56dc637fd81dfdb64df
Saygong <andreagentilini5@gmail.com>
2022-06-12 17:37:50 +0200

    fix: fixed moderator method accordings to the new signature

--- MandrilliWeb/src/app/features/profile-module/moderator-section/moderator-section.component.ts
+++ MandrilliWeb/src/app/features/profile-module/moderator-section/moderator-section.component.ts
@@ -23,7 +23,7 @@ export class ModeratorSectionComponent implements OnInit {
 
     public ban(username: string): void {
         try {
-            this.moderatorClient.banUser(this.userInSessionId, username);
+            this.moderatorClient.banUser(username);
         } catch (err) {
             console.log('An error occurred while banning a user: ' + err);
         }
@@ -32,7 +32,7 @@ export class ModeratorSectionComponent implements OnInit {
     public newModerator(usrnm: string, pwd: string): void {
         try {
             let loginInfo: LoginInfo = { username: usrnm, password: pwd };
-            this.moderatorClient.addModerator(this.userInSessionId, loginInfo).subscribe((data) => {
+            this.moderatorClient.addModerator(loginInfo).subscribe((data) => {
                 console.log('Correctly added: ' + data.username);
             });
         } catch (err) {
