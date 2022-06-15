4fb1685b664849a5feceaf9b016dd1432381f8e2
pierluigimarchioro <marchioropierluigi@gmail.com>
2022-06-15 23:05:15 +0200

    feat(client-api-services): added support for moderators/credentials endpoint in the moderator api service

--- MandrilliWeb/src/app/core/api/handlers/moderator-api.ts
+++ MandrilliWeb/src/app/core/api/handlers/moderator-api.ts
@@ -8,7 +8,8 @@ import { User } from '../../model/user/user';
 import { LoginInfo } from './auth-api';
 
 /**
- * Class that handles communication with moderator-related endpoints
+ * Class that handles communication with moderator-related endpoints.
+ * Such endpoints can be used only by a user with the Moderator role.
  */
 @Injectable({
     providedIn: 'root',
@@ -18,19 +19,41 @@ export class ModeratorApi extends BaseAuthenticatedApi {
         super(httpClient, accessTokenProvider);
     }
 
-    public addModerator(newModInfo: LoginInfo): Observable<User> {
+    /**
+     * Adds a new moderator with the provided credentials.
+     * Such credentials are temporary and will be updated by the new moderator
+     * in occasion of its first login.
+     * @param credentials
+     */
+    public addModerator(credentials: LoginInfo): Observable<User> {
         const reqPath: string = `${this.baseUrl}/api/moderators/additions`;
-        // TODO add moderator with newModInfo.status.temporary = true 
+
         return this.httpClient
-            .post<User>(reqPath, newModInfo, this.createRequestOptions())
+            .post<User>(reqPath, credentials, this.createRequestOptions())
             .pipe(catchError(this.handleError));
     }
 
+    /**
+     * Bans the user with the specified username
+     * @param username
+     */
     public banUser(username: string): Observable<void> {
         const reqPath: string = `${this.baseUrl}/api/moderators/bans`;
 
         return this.httpClient
-            .post<void>(reqPath, {username: username}, this.createRequestOptions())
+            .post<void>(reqPath, { username: username }, this.createRequestOptions())
+            .pipe(catchError(this.handleError));
+    }
+
+    /**
+     * Updates the credentials of a moderator after he performs its first login.
+     * @param newCredentials
+     */
+    public updateTemporaryCredentials(newCredentials: LoginInfo): Observable<void> {
+        const reqPath: string = `${this.baseUrl}/api/moderators/credentials`;
+
+        return this.httpClient
+            .put<void>(reqPath, newCredentials, this.createRequestOptions())
             .pipe(catchError(this.handleError));
     }
 }
